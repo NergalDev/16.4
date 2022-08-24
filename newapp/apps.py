c452bfdbb3f5465390c3047fedc22f5455e1148d
NergalDev <108280056+NergalDev@users.noreply.github.com>
2022-08-24 18:22:23 +0300

    Update apps.py

--- newapp/apps.py
+++ newapp/apps.py
@@ -16,16 +16,3 @@ red = redis.Redis(
     password='3AhbR1HWfwmnKftX15RqkoPH7SzcLPmI'
 )
 
-
-# redis = redis.Redis(
-#     host='localhost',
-#     port='6379')
-#
-# redis.set('mykey', 'Hello from Python!')
-# value = redis.get('mykey')
-# print(value)
-#
-# redis.zadd('vehicles', {'car': 0})
-# redis.zadd('vehicles', {'bike': 0})
-# vehicles = redis.zrange('vehicles', 0, -1)
-# print(vehicles)
